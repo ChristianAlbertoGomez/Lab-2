a659397d9a4870a525364c84c7ecce9fa5d630f0
ChristianAlbertoGomez <cagomez15@miners.utep.edu>
2022-03-02 11:37:57 -0700

    Fixed segmentation error in sw and lw instructions.

--- riscv.c
+++ riscv.c
@@ -8,9 +8,17 @@
 
 int32_t* reg; // Array of 32 32-bit registers
 
+char* file_name = "mem.txt";
+
 void init_regs();
 bool interpret(char* instr);
 void write_read_demo();
+int converterAtoi(char* command);
+int findCommand(char* command);
+int compareCommand(char* command,char* inputUser);
+int offsetValue(char* string);
+int baseValue(char* string);
+char* get_baseToken(char* instr);
 
 /**
  * Initialize register array for usage.
@@ -43,7 +51,11 @@ void print_regs(){
  * as a parameter to this function.
  */
 bool interpret(char* instr){
+
   char delimeters[] = {"ADD,X ADDI"};
+  char delimetersTwo[] = {"SWORD,LW () X"};
+  char baseDelim[] = {"SWORD,LW"};
+
   char command[strlength(instr)];
 
   int i;
@@ -53,13 +65,18 @@ bool interpret(char* instr){
 
 
   char* token = strtok(command,delimeters);
+
   long int rs1;
   long int rs2;
-  char* mem_file = "mem.txt";
+  long int rd;
+  long int base;
+  long int address;
 
   if(findCommand(instr)==1){
      printf("This is ADD instruction: \n");
 
+     rd = converterAtoi(token);
+
      token = strtok(NULL,delimeters);
      rs1 = converterAtoi(token);
 
@@ -67,11 +84,20 @@ bool interpret(char* instr){
      rs2 = converterAtoi(token);
 
      long int total = rs1+rs2;
+
+      reg[rd] = total;
+
+
      printf("Total of sum is:%ld \n",total);
+     print_regs();
+
+     return true;
 
    }else if(findCommand(instr)==2){
      printf("This is ADDI instructions \n");
 
+     rd = converterAtoi(token);
+
      token = strtok(NULL,delimeters);
      rs1 = converterAtoi(token);
 
@@ -79,11 +105,76 @@ bool interpret(char* instr){
      rs2 = converterAtoi(token);
 
      long int total = rs1+rs2;
+     reg[rd] =  total;
+
      printf("Total of sum is:%ld \n",total);
+     print_regs();
+     return true;
+
+   }else if(findCommand(instr)==3){
+     printf("This is LW instructions:  \n");
+
+     char* lwToken = strtok(instr,delimetersTwo);
+
+     rd = converterAtoi(lwToken);
+     printf("%d \n",rd);
+
+     lwToken = strtok(NULL,delimetersTwo);
+     long int offset = atoi(lwToken)*4;
+     printf("%s:%ld \n",lwToken,offset);
+
+     lwToken = strtok(NULL,delimetersTwo);
+     base = atoi(lwToken);
+     printf("%s:%ld \n",lwToken,base);
+
+     address = base+offset;
+     reg[rd] = read_address(address,file_name);
+
+     print_regs();
+     return true;
+
+   }else if(findCommand(instr)==4){
+     printf("This is SW instructions \n");
+     char* swToken = strtok(instr,delimetersTwo);
+
+     rd = converterAtoi(swToken);
+     printf("%d \n",rd);
+
+     swToken = strtok(NULL,delimetersTwo);
+     long int offset = atoi(swToken)*4;
+     printf("%s:%ld \n",swToken,offset);
+
+     swToken = strtok(NULL,delimetersTwo);
+     base = atoi(swToken);
+     printf("%s:%ld \n",swToken,base);
+
+     address = base+offset;
+     write_address(rd,address,file_name);
+
+     print_regs();
+     return true;
 
    }else{
      printf("Nothing \n");
+     return false;
    }
+}
+
+//FIX THIS METHOD
+char* get_baseToken(char* instr){
+  char baseDelim[] = {"SW,LW"};
+  char command[strlength(instr)];
+
+  int i;
+  for(i=0;i<strlength(instr);i++){
+    command[i] = instr[i];
+  }
+
+  char* baseToken = strtok(command,baseDelim);
+
+  baseToken = strtok(NULL,baseDelim);
+
+  return baseToken;
 }
 
 int converterAtoi(char* command){
@@ -101,13 +192,20 @@ int findCommand(char* command){
 
      while(*temp_tokenizer){
       if(compareCommand("ADD",*temp_tokenizer)==1){
-        return 1;
-      }else if(compareCommand("ADDI",*temp_tokenizer)==1){
-        return 2;
-      }else{
-        return 0;
-      }
-      temp_tokenizer++;
+          return 1;
+       }//1
+      if(compareCommand("ADDI",*temp_tokenizer)==1){
+          return 2;
+       }//2
+      if(compareCommand("LW",*temp_tokenizer)==1){
+          return 3;
+       }//3
+      if(compareCommand("SWORD",*temp_tokenizer)==1){
+          return 4;
+       }//4
+      else{
+          return 0;
+       }//else
      }
 }
 
@@ -115,15 +213,54 @@ int compareCommand(char* command,char* inputUser){
    int i;
 
    for(i=0;command[i]!='\0'&&inputUser[i]!='\0';i++);
-     if(command[i]<inputUser[i]){
+
+   if(command[i]<inputUser[i]){
        return 0;
-     }
-     else if(command[i]>inputUser[i]){
+    }
+   else if(command[i]>inputUser[i]){
        return 0;
-     }
-     else{
+    }
+   else{
        return 1;
-     }
+    }
+}
+
+int offsetValue(char* string){
+   int length = strlength(string);
+   int i=0;
+   int j;
+   int immediateValue;
+
+   while(string[i] != '('){
+     i++;
+   }
+
+   char immediate[length-i];
+
+   for(j=0;j<(length-i);j++){
+     immediate[j] = string[j];
+   }
+    immediateValue = atoi(immediate);
+    return immediateValue;
+}
+int baseValue(char* string){
+  int length = strlength(string);
+  int i=0;
+  int j;
+  int immediateValue;
+
+   while(string[i]!='X'){
+     i++;
+   }
+     i++;
+
+   char* immediate = (char*)malloc(length-1);
+
+   for(j=0;j<(length-i);j++){
+       immediate[j] = string[j-i];
+    }
+    immediateValue = atoi(immediate);
+    return immediateValue;
 }
 
 
